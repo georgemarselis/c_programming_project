01a1ca762e0547b1b2aa9d30e0aa2dc65327d3c7
George Marselis <george@marsel.is>
2015-12-28 02:34:46 +0200

    Exercise 4: rational_manipulation.c compiles, but no real content

--- exercise_4/rational_manipulation.c
+++ exercise_4/rational_manipulation.c
@@ -27,3 +27,149 @@
 
 Υλοποιήστε κυρίως πρόγραμμα που να τεστάρει τις συγκεκριμένες συναρτήσεις.
 */
+
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <sys/stat.h>
+
+// function prototypes
+int    sanity_ok(  void );
+void   initialize( void );
+size_t read_table( void );
+void   help( void );
+void   parse_command_args( int argc, char *argv[] );
+void   begin_execution( void );
+
+///////////////////////
+
+
+struct command_line {
+	char *filename;
+};
+
+struct command_line args = { NULL };
+
+int sanity_ok( void )
+{
+
+	int sanity_status = 1;
+
+	if( !setlocale( LC_CTYPE, "" ) ) {
+		fprintf( stderr, "LC_CTYPE not set! Please set appropriatelly and run again.\n" );
+		sanity_status = 0;
+	}
+
+	return sanity_status;
+}
+
+
+void initialize ( void )
+{
+
+	unsigned long bytes_read = 0;
+
+	bytes_read = read_table( );
+	if( !bytes_read ) {
+		fprintf( stderr, "No text loaded for processing." );
+		exit( -1 );
+	}
+
+	return;
+}
+
+
+size_t read_table( void )
+{
+
+	size_t bytes_read = 0;
+
+	return bytes_read;
+
+}
+
+
+void help( void ) 
+{
+	fprintf( stderr, "ΠΕΖ2015: Άσκηση 3η: Απαρήθμηση και αναζήτηση λέξεων.\n\n" );
+	fprintf( stderr, "Επιλογές:\n" );
+	fprintf( stderr, "\t-h Προβολή βοήθειας (αυτή εδώ που βλέπετε)\n");
+	fprintf( stderr, "\t-f <αρχειο> Επιλογή αρχείου για αναζήτηση (προεπιλογή: \"lorem_ipsum.txt\"\n");
+
+
+	return;
+}
+
+
+void parse_command_args( int argc, char *argv[] )
+{
+	char c = 0;
+
+	opterr = 0;
+	while( ( c = getopt (argc, argv, "hf:" ) ) != -1) {
+		switch (c) {
+			case 'f':
+				if( NULL == optarg ) {
+					fprintf( stderr, "Filename not set. Exiting!\n");
+					exit( -1 );
+				}
+				args.filename = optarg;
+				break;
+			case 'h':
+				help( );
+				exit( -1 );
+				break;
+			case '?':
+				if( 'f' == optopt ) {
+					fprintf( stderr, "Option -%c requires an argument.\n", optopt );
+					exit( -1 ) ;
+				}
+				else if( isprint( optopt ) ) {
+					fprintf( stderr, "Unknown option `-%c'.\n", optopt );
+					exit( -1 );
+				}
+				break;
+			default:
+				help( );
+				exit( -1 );
+				break;
+		}
+	}
+
+	return;
+}
+
+void begin_execution( void )
+{
+
+
+	return;
+}
+
+
+int main( int argc, char *argv[] )
+{
+
+	// check the status of the terminal
+	if( !sanity_ok( ) ) {
+		exit( EXIT_FAILURE );
+	}
+
+	// in case of emergency grake blass
+	if( argc > 1 ) {
+		parse_command_args( argc, argv );
+	}
+
+	begin_execution( );
+
+	return 0;
+}
